d0b719ded50411519bc24683c05ec8d37cd35729
Luis Felger <firefligher.tornado@gmail.com>
2023-02-19 15:46:34 +0100

    Add vm_to_user for RISC-V

--- module/arch/x86/vm_to_user.c
+++ module/arch/x86/vm_to_user.c
@@ -5,21 +5,19 @@
 
 void ptedit_arch_vm_to_user(ptedit_entry_t *user, vm_t *vm) {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
-#if CONFIG_PGTABLE_LEVELS > 4
-    if(vm->p4d) user->p4d = (vm->p4d)->p4d;
-#else
-#if !defined(__ARCH_HAS_5LEVEL_HACK)
-    if(vm->p4d) user->p4d = (vm->p4d)->pgd.pgd;
-#else
-    if(vm->p4d) user->p4d = (vm->p4d)->pgd;    
-#endif
-#endif
+  #if CONFIG_PGTABLE_LEVELS > 4
+    if (vm->p4d) user->p4d = (vm->p4d)->p4d;
+  #elif defined(__ARCH_HAS_5LEVEL_HACK)
+    if (vm->p4d) user->p4d = (vm->p4d)->pgd;
+  #else
+    if (vm->p4d) user->p4d = (vm->p4d)->pgd.pgd;
+  #endif
 #endif
 
-  if(vm->pgd) user->pgd = (vm->pgd)->pgd;
-  if(vm->pmd) user->pmd = (vm->pmd)->pmd;
-  if(vm->pud) user->pud = (vm->pud)->pud;
-  if(vm->pte) user->pte = (vm->pte)->pte;
+  if (vm->pgd) user->pgd = (vm->pgd)->pgd;
+  if (vm->pmd) user->pmd = (vm->pmd)->pmd;
+  if (vm->pud) user->pud = (vm->pud)->pud;
+  if (vm->pte) user->pte = (vm->pte)->pte;
 
   user->valid = vm->valid;
 }
