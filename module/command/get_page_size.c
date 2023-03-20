7d69f762fbe040e0eeb0d431042501fd592ce1b6
Luis Felger <firefligher.tornado@gmail.com>
2023-03-20 06:59:31 +0100

    Add implementation for ptedit_command_get_page_size

--- module/command/get_page_size.c
+++ module/command/get_page_size.c
@@ -1,8 +1,9 @@
+#include "../arch/arch.h"
 #include "command.h"
 
 long ptedit_command_get_page_size(
   unsigned int ioctl_num,
   unsigned long ioctl_param
 ) {
-    return -1;
+    return real_page_size;
 }
