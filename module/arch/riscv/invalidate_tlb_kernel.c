88c24a298b37e3429a9f0c2e7b5956a0b6db432a
Luis Felger <firefligher.tornado@gmail.com>
2023-03-22 19:14:12 +0100

    Improve userland library code

--- module/arch/riscv/invalidate_tlb_kernel.c
+++ module/arch/riscv/invalidate_tlb_kernel.c
@@ -2,5 +2,5 @@
 
 void ptedit_arch_invalidate_tlb_kernel(void *addr) {
   pr_warn("Running experimental ptedit_arch_invalidate_tlb_kernel implementation.\n");
-  __asm__ __volatile__ ("sfence.vma" : : : "memory");
+  asm volatile("sfence.vma" ::: "memory");
 }
