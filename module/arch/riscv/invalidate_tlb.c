d218dd73375ccfdf2f9c60fdb001d62b96e7eb81
Luis Felger <firefligher.tornado@gmail.com>
2023-03-23 17:50:43 +0100

    Improve tlb_flush

--- module/arch/riscv/invalidate_tlb.c
+++ module/arch/riscv/invalidate_tlb.c
@@ -1,6 +1,33 @@
+#include <asm/current.h>
+#include <asm/mmu_context.h>
+#include <linux/printk.h>
+#include <linux/sched.h>
+
 #include "../arch.h"
 
+/**
+ * Vaguely adopted from
+ * https://github.com/Tina-Linux/tina-d1x-linux-5.4/blob/master/arch/riscv/mm/tlbflush.c
+ */
+
 void ptedit_arch_invalidate_tlb(void *addr) {
-    /* FIXME. */
-    pr_alert("ptedit_arch_invalidate_tlb is unimplemented.\n");
+  pid_t pid;
+  struct mm_struct *mm;
+  int asid;
+
+  if (!ptedit_mm_acquire(&mm, pid = task_pid_nr(current))) {
+    pr_warn("Cannot invalidate TLB with kernel strategy.\n");
+    return;
+  }
+
+  asid = cpu_asid(mm);
+
+  /*
+   * NOTE:  Although the Tina Kernel aligns the virtual address (addr) to a
+   *        page boundary first, acccording to the RISC-V manual, this is not
+   *        required. Hence, we omit it here.
+   */
+
+  asm volatile("sfence.vma %0, %1" :: "r"(addr), "r"(asid) : "memory");
+  ptedit_mm_release(pid);
 }
--- module/arch/riscv/invalidate_tlb_kernel.c
+++ module/arch/riscv/invalidate_tlb_kernel.c
@@ -1,6 +1,10 @@
 #include "../arch.h"
 
 void ptedit_arch_invalidate_tlb_kernel(void *addr) {
-  pr_warn("Running experimental ptedit_arch_invalidate_tlb_kernel implementation.\n");
-  asm volatile("sfence.vma" ::: "memory");
+  /*
+   * At least on the Tina Kernel, there is no accessible flush function from
+   * the Kernel, that we could call here. Instead, we rely on our own.
+   */
+
+  ptedit_arch_invalidate_tlb(addr);
 }
